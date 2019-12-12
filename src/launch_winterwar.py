86d9c368376aa1b317413ddfdbba9c6202be33d2
Tuomo <tuokri@tuta.io>
2019-12-12 16:43:49 +0200

    Version 1.0

--- src/launch_winterwar.py
+++ src/launch_winterwar.py
@@ -2,11 +2,18 @@
 Purge cached Winter War mod files in Rising Storm 2
 workshop cache to avoid conflicts and run Rising Storm 2.
 """
+import argparse
 import ctypes.wintypes
+import errno
 import os
+import shutil
+import subprocess
 import sys
+import winreg
+from argparse import Namespace
 from pathlib import Path
 from typing import List
+from winreg import HKEY_LOCAL_MACHINE
 
 import logbook
 from logbook import Logger
@@ -14,13 +21,17 @@ from logbook import RotatingFileHandler
 from logbook import StreamHandler
 
 VNGAME_EXE = "VNGame.exe"
+VNGAME_EXE_PATH = Path(
+    "steamapps\\common\\Rising Storm 2\\Binaries\\Win64") / Path(VNGAME_EXE)
 WW_PACKAGE = "WinterWar.u"
 WW_WORKSHOP_ID = 1758494341
-AUDIO_DIR = "WwiseAudio"
+AUDIO_DIR = Path("CookedPC\\WwiseAudio")
 
 # Windows constants.
 CSIDL_PERSONAL = 5  # My Documents.
 SHGFP_TYPE_CURRENT = 0  # Get current, not default value.
+STEAM_REG_32 = "SOFTWARE\\Valve\\Steam"
+STEAM_REG_64 = "SOFTWARE\\Wow6432Node\\Valve\\Steam"
 
 
 def user_documents_dir() -> Path:
@@ -35,11 +46,11 @@ USER_DOCS_DIR = user_documents_dir()
 CACHE_DIR = USER_DOCS_DIR / Path("My Games\\Rising Storm 2\\ROGame\\Cache")
 PUBLISHED_DIR = USER_DOCS_DIR / Path("My Games\\Rising Storm 2\\ROGame\\Published")
 LOGS_DIR = USER_DOCS_DIR / Path("My Games\\Rising Storm 2\\ROGame\\Logs")
+SCRIPT_LOG_PATH = LOGS_DIR / Path("LaunchWinterWar.log")
 
 logbook.set_datetime_format("local")
 _sh = StreamHandler(sys.stdout, level="INFO")
-_rfh = RotatingFileHandler(
-    LOGS_DIR / Path("LaunchWinterWar.log"), level="INFO", bubble=True)
+_rfh = RotatingFileHandler(SCRIPT_LOG_PATH, level="INFO", bubble=True)
 _rfh.format_string = (
     "[{record.time}] {record.level_name}: {record.channel}: "
     "{record.func_name}(): {record.message}"
@@ -71,7 +82,54 @@ def find_ww_cache_dirs() -> List[Path]:
     return list(set(cache_dirs))
 
 
+def find_steam_install_dir() -> Path:
+    """Read Steam installation directory from Windows registry."""
+    steam_reg_keys = [STEAM_REG_32, STEAM_REG_64]
+
+    for steam_reg_key in steam_reg_keys:
+        logger.info("trying to read key: {k}", k=steam_reg_key)
+        try:
+            with winreg.OpenKey(HKEY_LOCAL_MACHINE, steam_reg_key) as key:
+                try:
+                    return Path(winreg.QueryValueEx(key, "InstallPath")[0])
+                except OSError as ose:
+                    if ose.errno != errno.ENOENT:
+                        raise
+        except FileNotFoundError:
+            logger.info("key not found: {k}", k=steam_reg_key)
+
+    raise RuntimeError("Could not find Steam directory")
+
+
+def find_vngame_exe() -> Path:
+    steam_dir = find_steam_install_dir()
+    vngame_exe_path = steam_dir / VNGAME_EXE_PATH
+
+    logger.info("looking for '{exe}'", exe=vngame_exe_path)
+    if not vngame_exe_path.exists():
+        raise RuntimeError(f"Could not find '{VNGAME_EXE}'")
+
+    return vngame_exe_path
+
+
+def error_handler(function, path, exc_info):
+    logger.error("function={f} path={p}",
+                 f=function, p=path, exc_info=exc_info)
+
+
+def parse_args() -> Namespace:
+    ap = argparse.ArgumentParser()
+    ap.add_argument(
+        "--dry-run", dest="dry_run",
+        action="store_true", default=False,
+        help="run without deleting files or launching Rising Storm 2",
+    )
+    return ap.parse_args()
+
+
 def main():
+    args = parse_args()
+
     logger.info("user documents directory: '{uh}'", uh=USER_DOCS_DIR)
     cache_dirs = find_ww_cache_dirs()
     count = len(cache_dirs)
@@ -79,6 +137,45 @@ def main():
         "found {count} Winter War cache directo{p}", count=count,
         p="ry" if count == 1 else "ries")
 
+    audio_dir = PUBLISHED_DIR / AUDIO_DIR
+    if audio_dir.exists():
+        logger.info("found audio directory: '{ad}'", ad=audio_dir)
+        cache_dirs.append(audio_dir)
+
+    for cache_dir in cache_dirs:
+        if args.dry_run:
+            logger.info("dry run, not removing: {cd}", cd=cache_dir)
+        else:
+            logger.info("removing: {cd}", cd=cache_dir)
+            shutil.rmtree(
+                cache_dir,
+                onerror=error_handler,
+            )
+
+    vngame_exe = find_vngame_exe()
+    logger.info("found Rising Storm 2 executable: '{exe}'", exe=vngame_exe)
+
+    if not args.dry_run:
+        logger.info("launching Rising Storm 2")
+        subprocess.Popen(str(vngame_exe))
+
 
 if __name__ == "__main__":
-    main()
+    try:
+        main()
+    except Exception as _e:
+        extra = (f"Check '{SCRIPT_LOG_PATH}' for more information."
+                 if SCRIPT_LOG_PATH.exists() else "")
+        ctypes.windll.user32.MessageBoxW(
+            0,
+            f"Error launching Winter War!\r\n"
+            f"{type(_e).__name__}: {_e}.\r\n"
+            f"{extra}\r\n",
+            "Error",
+            0
+        )
+        # noinspection PyBroadException
+        try:
+            logger.exception("error running script")
+        except Exception:
+            pass
